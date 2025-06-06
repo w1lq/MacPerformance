98508936d31d940fc2556e55826a99668ff9bc89
w1lq <mbartkowiak@gmail.com>
2025-06-06 15:13:03 +0200

    Update simulation script for Python3

--- Simulation.py
+++ Simulation.py
@@ -1,31 +1,34 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
 from TOSSIM import *
-from sets import Set
+try:
+    from sets import Set  # Python 2 fallback
+except ImportError:
+    Set = set
 import sys
 from optparse import OptionParser
 
 parser = OptionParser(usage="usage: %prog [options] filename",
-	version="%prog 1.0")
+    version="%prog 1.0")
 
 parser.add_option("-g", "--gainfile",
-	action="store",
-	dest="gainfile",
-	default="topology.txt",
-	help="file containing gains between simulation nodes")
+    action="store",
+    dest="gainfile",
+    default="topology.txt",
+    help="file containing gains between simulation nodes")
 
 parser.add_option("-n", "--noisefile",
-	action="store",
-	dest="noise",
-	default="Noise/meyer-heavy-short.txt",
-	help="file containing gains between simulation nodes")
+    action="store",
+    dest="noise",
+    default="Noise/meyer-heavy-short.txt",
+    help="file containing gains between simulation nodes")
 
 (options, args) = parser.parse_args()
 
 options_dict = vars(options)
 
-print options_dict['gainfile']
+print(options_dict['gainfile'])
 
-print "Simulation start"
+print("Simulation start")
 
 from tinyos.tossim.TossimApp import *
 n = NescApp()
@@ -40,18 +43,18 @@ gainfile = open(options_dict['gainfile'], "r")
 
 nodes = Set([])
 
-print "Simulation Topology:"
+print("Simulation Topology:")
 lines = gainfile.readlines()
 for line in lines:
-	splitlines = line.split() 
-	if (len(splitlines) > 0):
-		if (splitlines[0] == "gain"):
-			r.add(int(splitlines[1]), int(splitlines[2]), float(splitlines[3].replace(",",".")))
-			print "Source:", splitlines[1], "Destination:", splitlines[2], "Gain:", splitlines[3], "dBm";
-			nodes.add(int(splitlines[1]))
-			nodes.add(int(splitlines[2]))
+    splitlines = line.split() 
+    if (len(splitlines) > 0):
+        if (splitlines[0] == "gain"):
+            r.add(int(splitlines[1]), int(splitlines[2]), float(splitlines[3].replace(",",".")))
+            print("Source:", splitlines[1], "Destination:", splitlines[2], "Gain:", splitlines[3], "dBm")
+            nodes.add(int(splitlines[1]))
+            nodes.add(int(splitlines[2]))
 
-print "Number of nodes: " + str(len(nodes)) + ", nodes' ids:", nodes
+print("Number of nodes: " + str(len(nodes)) + ", nodes' ids:", nodes)
 
 # Allocating debug outputs
 energy_output = open("Simulation/Energy.txt", "w")
@@ -66,39 +69,39 @@ resultfile = open("Simulation/Result.txt", "w")
 noise = open(options_dict['noise'], "r")
 lines = noise.readlines()
 for line in lines:
-	stripline = line.strip() 
-	if (stripline != ""):
-		val = int(stripline)
-		for node in nodes:
-			t.getNode(node).addNoiseTraceReading(val)
+    stripline = line.strip() 
+    if (stripline != ""):
+        val = int(stripline)
+        for node in nodes:
+            t.getNode(node).addNoiseTraceReading(val)
 
 for node in nodes:
-	print "Creating noise model for node " + str(node) + "."
-	t.getNode(node).createNoiseModel()
+        print("Creating noise model for node " + str(node) + ".")
+        t.getNode(node).createNoiseModel()
 
 # Boot time spread
 boot_time = 0
 for node in nodes:
-	t.getNode(node).bootAtTime(0 + boot_time);
-	boot_time += 50000000 # equal to 5 ms
+    t.getNode(node).bootAtTime(0 + boot_time);
+    boot_time += 50000000 # equal to 5 ms
 
 
 # This runs the network for 50 seconds:
 time = t.time()
 while (time + t.ticksPerSecond() * 50 > t.time()):
-	t.runNextEvent()
+    t.runNextEvent()
 
 resultfile.write("%d\n" % (t.time()))
 
 for node in nodes:
-	m = t.getNode(node)
-	v = m.getVariable("MacPerformanceC.received_packets")
-	received_packets = v.getData()
-	c = m.getVariable("MacPerformanceC.counter")
-	sent_packets = c.getData()	
-	print "The node id", node, "has sent", sent_packets, "and received", received_packets, "in total.";	
+    m = t.getNode(node)
+    v = m.getVariable("MacPerformanceC.received_packets")
+    received_packets = v.getData()
+    c = m.getVariable("MacPerformanceC.counter")
+    sent_packets = c.getData()
+    print("The node id", node, "has sent", sent_packets, "and received", received_packets, "in total.")
 
-	resultfile.write("%d,%d,%d\n" % (node, sent_packets, received_packets))
+    resultfile.write("%d,%d,%d\n" % (node, sent_packets, received_packets))
 
-print "End of simulation."
+print("End of simulation.")
 
